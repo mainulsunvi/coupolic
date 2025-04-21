e4e49b18946dd23ef2a5c4c8eb395e37dae9934b
mainulsunvi <mainulsunvi@gmail.com>
2025-04-21 18:12:39 +0600

    changed line height

--- ui/src/pages/GeneralOptions.vue
+++ ui/src/pages/GeneralOptions.vue
@@ -1,5 +1,23 @@
 
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
 
 <template>
-	<h1>General Page</h1>
+	    <div class="inner-box">
+      <div class="inner-section-20">
+		<div class="coupolic-general-sub-nav-menus">
+			<div class="sub-nav-menu">
+				<RouterLink to="#">General</RouterLink>
+			</div>
+			<div class="sub-nav-menu">
+				<RouterLink to="#">Usage Restriction</RouterLink>
+			</div>
+			<div class="sub-nav-menu">
+				<RouterLink to="#">Usage Limit</RouterLink>
+			</div>
+		</div>
+	  </div>
+      <div class="inner-section-80">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error repellendus harum voluptatum, placeat distinctio, qui animi officiis odio ut quam nemo, dolorem suscipit nobis. Perferendis expedita ullam autem tempore dolore, fuga cum tenetur corrupti eaque dolor, dolores vel illum porro, nihil vero nostrum quis soluta quaerat laboriosam asperiores amet harum? Provident deserunt harum corrupti ea. Dolore voluptatem ad quis sapiente quaerat perspiciatis modi hic ab recusandae deserunt ipsum, itaque reprehenderit molestiae, fugit magni et natus laudantium dicta praesentium vitae. Rem sequi autem vitae dolorum quod porro, odio perspiciatis veritatis ad enim voluptate, alias voluptatum voluptas mollitia numquam. Asperiores fuga harum atque praesentium dicta excepturi eveniet reprehenderit ex. Inventore, at omnis dolorem corporis quas delectus dolore aliquid possimus quisquam eaque. Numquam!</div>
+    </div>
 </template>
